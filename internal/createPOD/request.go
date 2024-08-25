5e02e5d15661b7c1355a3c68cc80d8ce1f2c20cd
Karuch <talk159820@gmail.com>
2024-08-25 14:31:52 +0300

    change to envs also in request.go

--- internal/createPOD/request.go
+++ internal/createPOD/request.go
@@ -1,24 +1,23 @@
 package createPOD
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/Karuch/OpenshiftStorageAgent/internal/global"
 	"github.com/Karuch/OpenshiftStorageAgent/internal/logs"
-	"bytes"
 )
 
 func Request(CompletePVCsMap map[string]int64) ([]byte, error){
 	// Define variables
 
-	tokenFilePath := "/go/kubernetes/token.txt"
-	apiServer := "https://192.168.49.2:8443"
-	namespace := "default"
-	url := fmt.Sprintf("%s/api/v1/namespaces/%s/pods", apiServer, namespace)
+	url := fmt.Sprintf("%s/api/v1/namespaces/%s/pods", global.APIServer, global.Namespace)
 
-	token, err := os.ReadFile(tokenFilePath)
+	token, err := os.ReadFile(global.TokenFilePath)
 	if err != nil {
 		e.LogError(err)
 	}
@@ -39,7 +38,7 @@ func Request(CompletePVCsMap map[string]int64) ([]byte, error){
 
 	// Send req using http Client
 	client := &http.Client{}
-	fmt.Println("Sending request to create pod to:", apiServer)
+	fmt.Println("Sending request to create pod to:", global.APIServer)
 	resp, err := client.Do(req)
 	if err != nil {
 		e.LogError(err)
@@ -47,7 +46,9 @@ func Request(CompletePVCsMap map[string]int64) ([]byte, error){
 	defer resp.Body.Close()
 	// I have no idea why, kubernetes return empty response on failure in this request
 	// Check if the response status code is not 200
-	if resp.StatusCode != 201 && resp.StatusCode != 200 {
+	if resp.StatusCode == 409 {
+		return nil, fmt.Errorf("server return unexpected status code: 409, agent-pod already exist?")
+	} else if resp.StatusCode != 201 && resp.StatusCode != 200 {
 		return nil, fmt.Errorf("server return unexpected status code: %d", resp.StatusCode)
 	} else {
 		fmt.Println("response statuscode of agent-pod creation:", resp.StatusCode)
